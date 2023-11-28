27d1c0cf8f282ec4940a0e3a2d83aaf0a5ca3ed5
FancyStrittholt <fancypants@DESKTOP-TSFMQAQ>
2023-11-28 09:46:06 -0500

    added comments

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const freelancers = [
     price: 70,
   },
 ];
-
+// calling the addFreelancer function below
 const addFreelancerIntervalId = setInterval(addFreelancer, 1000);
 
 function render() {
@@ -56,7 +56,7 @@ function render() {
 
   for (const item of freelancers) {
     const row = createDataRow(item.name, item.occupation, item.price);
-
+    // adding new row to table
     if (row) {
       table.appendChild(row);
     }
@@ -64,57 +64,75 @@ function render() {
 }
 
 function createDataRow(name, occupation, price) {
+  // creating a new row
   const row = document.createElement("tr");
 
+  // Do I have this date row already in my table?
   const exists = document.getElementById(name + occupation + price);
 
+  // Since it is undefined it will not add and loop through to the next one
   if (exists) return;
 
+  // setting name column
   const tdName = document.createElement("td");
   const nameText = document.createTextNode(name);
   tdName.appendChild(nameText);
   row.appendChild(tdName);
 
+  // setting occupation column
   const tdOccupation = document.createElement("td");
   const occupationText = document.createTextNode(occupation);
   tdOccupation.appendChild(occupationText);
   row.appendChild(tdOccupation);
 
+  // setting price column
   const tdPrice = document.createElement("td");
   const priceText = document.createTextNode("$" + price);
   tdPrice.appendChild(priceText);
   row.appendChild(tdPrice);
 
+  // setting unique row id
   row.id = name + occupation + price;
 
+  // returning newly created row
   return row;
 }
 
 function addFreelancer() {
+  // rendering the table
   render();
 
+  // setting a max
   if (freelancers.length >= 20) {
     clearInterval(addFreelancerIntervalId);
   }
 
+  // creating new random freelancer and adding it
   freelancers.push({
     name: possibleNames[Math.floor(Math.random() * possibleNames.length)],
     occupation:
       possibleOccupations[
         Math.floor(Math.random() * possibleOccupations.length)
       ],
+
+    // randomizing the starting price with a max of 100
     price: Math.floor(Math.random() * 100),
   });
 
+  // calculating the average in the table
   calcAverage();
 }
-
+//
 function calcAverage() {
+  // grabbing the average placeholder id
   const element = document.querySelector("#average");
+  // calculating the average
   const average =
     freelancers.reduce((acc, curr) => acc + curr.price, 0) / freelancers.length;
+  // creating text node
   const text = document.createTextNode(
     `The average starting price is $${Math.floor(average)}.`
   );
+  // replacing that number everytime a new freelancer is added
   element.replaceChildren(text);
 }
